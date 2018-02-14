dd5fda3db5bacc698dbf464718df39a1bb9255fb
cyrrrchan <crystalchan110@gmail.com>
2018-02-14 00:09:54 -0800

    2018.02.14

---
+++ ThesisPrototype_01/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BACKINGTRACK = 3055447710U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BASS = 1291433344U;
+        static const AkUniqueID FX = 1802970371U;
+        static const AkUniqueID HIHAT = 3437592667U;
+        static const AkUniqueID KICK = 2181839183U;
+        static const AkUniqueID LIVEINSTRUMENTS = 2550428931U;
+        static const AkUniqueID LOOP = 691006007U;
+        static const AkUniqueID SNARE = 3607016552U;
+        static const AkUniqueID STRINGS = 3363523641U;
+        static const AkUniqueID SYNTH = 3928024197U;
+        static const AkUniqueID VOICE = 3170124113U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID BACKINGTRACK = 4202636045U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID COMMUNICATION = 530303819U;
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
---
+++ ThesisPrototype_01/Assets/__Scripts/GameController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public AudioSource _bass;
+    public AudioSource _fx;
+    public AudioSource _hiHat;
+    public AudioSource _kick;
+    public AudioSource _liveInstruments;
+    public AudioSource _loop;
+    public AudioSource _snare;
+    public AudioSource _strings;
+    public AudioSource _synth;
+    public AudioSource _voice;
+
+    private bool _hitSomething = false;
+    private string _starTag;
+
+    // Use this for initialization
+    void Start () {
+        /*AkSoundEngine.SetRTPCValue("Bass", 50.0f);
+        AkSoundEngine.SetRTPCValue("FX", 50.0f);
+        AkSoundEngine.SetRTPCValue("HiHat", 50.0f);
+        AkSoundEngine.SetRTPCValue("Kick", 50.0f);
+        AkSoundEngine.SetRTPCValue("LiveInstruments", 50.0f);
+        AkSoundEngine.SetRTPCValue("Loop", 50.0f);
+        AkSoundEngine.SetRTPCValue("Snare", 50.0f);
+        AkSoundEngine.SetRTPCValue("Strings", 50.0f);
+        AkSoundEngine.SetRTPCValue("Synth", 50.0f);
+        AkSoundEngine.SetRTPCValue("Voice", 50.0f);*/
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+        Debug.DrawRay(transform.position, forward, Color.green);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Untagged")
+                _hitSomething = false;
+
+            else if (hit.collider.tag != "Untagged")
+            {
+                _starTag = hit.collider.tag;
+                _hitSomething = true;
+            }
+        }
+
+        if (_hitSomething)
+            ChangeSound();
+    }
+
+    void ChangeSound()
+    {
+        if (_starTag == "Bass")
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0)) //if you press the left mouse button
+            {
+                _bass.volume += 0.1f;
+
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Mouse1)) //if you press the left mouse button
+            {
+                _bass.volume -= 0.1f;
+            }
+        }
+
+        else if (_starTag == "FX")
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0)) //if you press the left mouse button
+            {
+                _fx.volume += 0.1f;
+
+            }
+
+            else if (Input.GetKeyDown(KeyCode.Mouse1)) //if you press the left mouse button
+            {
+                _fx.volume -= 0.1f;
+            }
+        }
+
+        else if (_starTag == "HiHat")
+        {
+            if (Input.GetKeyDown(KeyCode.Q)) //if you press the left mouse button
+            {
+                _hiHat.volume = 0.1f;
+            }
+
+            else if (Input.GetKeyDown(KeyCode.E)) //if you press the left mouse button
+            {
+                _hiHat.volume -= 0.1f;
+            }
+        }
+    }
+}
